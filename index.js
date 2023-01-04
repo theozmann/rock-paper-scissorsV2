5cfb8b3fa0b4cc025691cb5c354bd640b09dbaa2
Austin Williams <ozmancoding@gmail.com>
2023-01-04 18:36:57 -0500

    Add playRound()

--- index.js
+++ index.js
@@ -13,4 +13,23 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+function playerChoice() {
+    let playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    return playerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper' || 
+        playerSelection === 'paper' && computerSelection === 'scissors' || 
+        playerSelection === 'scissors' && computerSelection === 'rock') {
+            console.log(`Sorry! ${computerSelection} beats ${playerSelection}, you lost!`);
+        }
+    else if (playerSelection === computerSelection) {
+        console.log(`You both chose ${playerSelection}! It's a tie!`);
+    }
+    else {
+        console.log(`Congrats! ${playerSelection} beats ${computerSelection}! You won!`);
+    }
+}
+
+console.log(playRound(playerChoice(), getComputerChoice()));
